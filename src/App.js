4d88558e9d20ad4de1dc1da993d3718de346fd0a
DESKTOP-QM1U137\tiennd <tien1241999@gmail.com>
2021-11-23 13:22:12 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Body } from "./components/Body"
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
